1d9db333c98bd9767ed00362886c41d962e4f473
AtharvaChobe <103590543+AtharvaChobe@users.noreply.github.com>
2024-02-24 00:37:02 +0530

    feat:added default value of input

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import {BsSearch} from 'react-icons/bs'
+import { BsSearch } from 'react-icons/bs'
 
 
 const Page = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState("space");
   const [results, setResults] = useState([]);
   const [num, setNum] = useState(1);
   const [orientation, setorientation] = useState("landscape")
@@ -20,51 +20,62 @@ const Page = () => {
       });
   };
 
+  useEffect(() => {
+    fetchData();
+  }, [orientation])
+
+
   return (
     <>
-    {/* Jumbotron */}
+      {/* Jumbotron */}
       <div className="flex items-center flex-row flex-wrap justify-evenly py-3 px-1 bg-slate-300 w-[100vw]">
         <span className='flex items-center my-2'>
-        <img className='w-16 h-16' src="https://veerle.duoh.com/images/design/_squareNormal/unsplash.png" alt="" />
-        <h1 className='font-bold text-3xl ml-1 items-center'>Unsplash</h1>
+          <img className='w-16 h-16' src="https://veerle.duoh.com/images/design/_squareNormal/unsplash.png" alt="" />
+          <h1 className='font-bold text-3xl ml-1 items-center'>Unsplash</h1>
         </span>
         <div className="flex flex-row items-center my-s">
           <input className='py-1 px-1 text-xl rounded-md' placeholder='Search an image...' type="text" value={input} onChange={(e) => { setInput(e.target.value) }} />
-          <button className='text-xl px-7 py-1' onClick={() => { fetchData(); }}> <BsSearch/> </button>
+          <button className='text-xl px-7 py-1' onClick={() => { fetchData(); }}> <BsSearch /> </button>
         </div>
         <select
-            className='text-lg px-3 py-2 rounded my-2'
-            value={orientation}
-            onChange={(e) => setorientation(e.target.value)}
-          >
-            <option value="landscape">Landscape</option>
-            <option value="portrait">Portrait</option>
-            <option value="squarish">Squarish</option>
-          </select>
+          className='text-lg px-3 py-2 rounded my-2'
+          value={orientation}
+          onChange={(e) => setorientation(e.target.value)}
+        >
+          <option value="landscape">Landscape</option>
+          <option value="portrait">Portrait</option>
+          <option value="squarish">Squarish</option>
+        </select>
       </div>
 
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 w-[90%] p-7 mx-[5%]'>
         {
           results.map((item) => (
-            
-            <a target="_blank" href={item.links.html}><img key={item.id} src={item.urls.regular} alt="" /></a>
-              
 
-            
+            <a target="_blank" href={item.links.html}><img key={item.id} src={item.urls.regular} alt="" /></a>
+
+
+
           ))
         }
       </div>
-      
-      <button
-        id='load_button'
-        className='bg-black text-white text-xl m-4 px-3 py-1 hidden text-center mx-auto'
-        onClick={() => {
-          setNum((prevNum) => prevNum + 1);
-          fetchData();
-        }}
-      >
-        Load more
-      </button>
+
+      {
+        input == ""
+          ?
+          ' '
+          :
+          <button
+            id='load_button'
+            className='bg-black text-white text-xl m-4 px-3 py-1 hidden text-center mx-auto'
+            onClick={() => {
+              setNum((prevNum) => prevNum + 1);
+              fetchData();
+            }}
+          >
+            Load more
+          </button>
+      }
     </>
   );
 };
